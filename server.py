8f1538d6216d3c7032923c4f2697e2366f80979d
Chris Tsui <chris.tsui112@outlook.com>
2020-09-24 18:36:21 -0700

    rebuild commit

--- server.py
+++ server.py
@@ -7,10 +7,10 @@ app = Flask(__name__, static_folder='./build', static_url_path='/')
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
-    
+
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
 
 if __name__ == "__main__":
-    app.run()
+     app.run(host='0.0.0.0', port=os.environ.get('PORT', 80))
