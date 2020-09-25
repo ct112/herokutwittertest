f34ff53059e172aeb612864f1aa5c4402dfc641a
Chris Tsui <chris.tsui112@outlook.com>
2020-09-25 08:16:37 -0700

    added error handler in api

--- server.py
+++ server.py
@@ -5,6 +5,10 @@ import os
 
 app = Flask(__name__, static_folder='./build', static_url_path='/')
 
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
@@ -14,4 +18,5 @@ def get_current_time():
     return {'time': time.time()}
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 5000))
+    app.run()
+    # (host='0.0.0.0', debug=False, port=os.environ.get('PORT', 5000))
