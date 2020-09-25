27678442f3e7053667c047d795ad40dcf97e8120
Chris Tsui <chris.tsui112@outlook.com>
2020-09-24 20:51:37 -0700

    changed port to 8080

--- server.py
+++ server.py
@@ -13,4 +13,4 @@ def get_current_time():
     return {'time': time.time()}
 
 if __name__ == "__main__":
-     app.run(host='0.0.0.0', port=os.environ.get('PORT', 80))
+     app.run(host='0.0.0.0', port=os.environ.get('PORT', 8080))
