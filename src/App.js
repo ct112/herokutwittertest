7eba8d992fefd4be7b4c37f0c74c5a01f1f4e85c
Chris Tsui <chris.tsui112@outlook.com>
2020-09-24 18:59:22 -0700

    changed proc

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        ... no changes in this part ... sure
+        ... no changes in this part ... sure bob
 
         <p>The current time is {currentTime}.</p>
       </header>
