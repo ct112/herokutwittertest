47b6c9b0496e27733fd61a9a82b49284d93faefe
Chris Tsui <chris.tsui112@outlook.com>
2020-09-24 18:33:45 -0700

    setup done

---
+++ server.py
@@ -0,0 +1,16 @@
+import time
+from flask import Flask
+import os
+
+app = Flask(__name__, static_folder='./build', static_url_path='/')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+    
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
+if __name__ == "__main__":
+    app.run()
